ec7edabc90c001426f78590d43406a02e13fab97
LazyIs <88606711+LazyIs@users.noreply.github.com>
2025-04-17 22:07:15 -0700

    Add files via upload

---
+++ Config.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct SupabaseConfig {
+    static let baseURL = "https://iliwrvyvsulblvzzeqob.supabase.co"
+    static let apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsaXdydnl2c3VsYmx2enplcW9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4NjIxOTIsImV4cCI6MjA2MDQzODE5Mn0.0gUA9Ltm49wYeocVm6zn267cvSlBqxiySXz1HGQRC8M"
+    static let authToken: String? = nil  // You can replace this with a real token if needed
+}
